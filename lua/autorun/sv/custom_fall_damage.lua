a8bf66590bcffbf10d43c4e8e30c10e5e711a6d8
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-06-01 15:27:06 -0400

    Initial commit

---
+++ lua/autorun/sv/custom_fall_damage.lua
@@ -0,0 +1,23 @@
+local SPEED_MULTIPLIER = 0.2418
+local SPEED_OFFSET = 141.75
+
+function calculateDamage( ply, speed )
+	local damage = ( SPEED_MULTIPLIER * speed ) - SPEED_OFFSET
+	local damage = math.max( 0, math.ceil( damage ) )
+	
+	return damage
+end
+
+function getFallDamage( ply, speed )
+	local damage = calculateDamage( ply, speed )
+	
+	return damage
+end
+
+-- Bastards arbitrarily returning "10" before we can get to it
+hook.Remove("GetFallDamage", "prone.FallDamage")
+hook.Remove("GetFallDamage", "Get Fall Damage")
+
+
+hook.Remove("GetFallDamage", "CFC_FallDamage")
+hook.Add("GetFallDamage", "CFC_FallDamage", getFallDamage)
