e1d85adb7a7da960529675518b828e554c462b72
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-10-07 17:12:15 -0400

    Whitespace fixes

--- lua/autorun/server/sv_custom_fall_damage.lua
+++ lua/autorun/server/sv_custom_fall_damage.lua
@@ -1,21 +1,21 @@
 local SPEED_MULTIPLIER = 0.175
 local SPEED_OFFSET = 141.75
 
---[[Default Values 
-Mult: 0.2418 
+-- [[Default Values
+Mult: 0.2418
 offset: 141.75
---]]
+-- ]]
 
 local function calculateDamage( ply, speed )
     local damage = ( SPEED_MULTIPLIER * speed ) - SPEED_OFFSET
     damage = math.max( 0, math.ceil( damage ) )
-    
+
     return damage
 end
 
 local function getFallDamage( ply, speed )
     local damage = calculateDamage( ply, speed )
-    
+
     return damage
 end
 
